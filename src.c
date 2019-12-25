27549584b371043cc0f8e936b03c7da734ba5ed7
czplwsk <czaplewska.eliza@gmail.com>
2019-12-25 16:40:24 +0100

    latest changes

--- src.c
+++ src.c
@@ -1,8 +1,11 @@
+#define _XOPEN_SOURCE 600
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <conio.h>
+#include <sys/types.h>
+#include <unistd.h> 
 
 struct account{
     char id[8];
@@ -10,24 +13,25 @@ struct account{
     bool in;
 }list[3];
 
+//struct account *logged = malloc(3*sizeof(struct account));
 int authorize(char* login,char* password,int c );
 void user();
 void interface();
 
 
-int main(){
+int main(int argc, char* argv[]){
     int i=0;
     FILE *fp;
     fp = fopen("dane.txt","r");
     setbuf(fp, NULL);
-    //struct account list[3];
     while(!feof(fp)){
         fscanf(fp,"%s",list[i].id);
         fscanf(fp,"%s",list[i].password);
         list[i].in = 0;
-        i+=1;
-    }
+        i+=1;}
     interface(list);
+    //if(fork()==0)
+    //{execv("141204_k.c",NULL);}
     fclose(fp);
 }
 
@@ -95,5 +99,6 @@ void user(){
             fflush(stdin);
             authorize(id, password,0);
             break;
-    }
+        case '3':
+            break;}
     return;}
