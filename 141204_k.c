18ad9f570d57cf33fefefd9aecd06b6e2466bbe5
czplwsk <czaplewska.eliza@gmail.com>
2020-01-15 13:06:10 +0100

    display groups

--- 141204_k.c
+++ 141204_k.c
@@ -17,6 +17,7 @@ struct msg_buf{
 
 struct msg_get{
   long mpid;
+  int mres;
   char mtext[100];
 }msg_rec;
 
@@ -26,38 +27,65 @@ void clean_stdin(){
         c = getchar();
     } while (c != '\n' && c != EOF);}
 
+void group();
 void user();
-void zaloguj();
 void interface();
 
 int main(int argc, char* argv[]){
     int mid = msgget(4444,0644|IPC_CREAT);
-    zaloguj(mid);
-    interface(mid);
+    char id[8];
+    while(!msg_rec.mres){
+      printf("Podaj login: \n");
+      scanf("%s",id);
+      clean_stdin();
+      strcpy(msg_pid.mtext,id);
+      msg_pid.mtype = 1000;
+      msg_pid.mpid = getpid();
+      msgsnd(mid,&msg_pid,sizeof(msg_pid)-sizeof(long),0);
+      msgrcv(mid,&msg_rec,sizeof(msg_rec)-sizeof(long),getpid(),0);
+      printf("%s\n",msg_rec.mtext );
+      fflush(stdout);}
+    interface(mid,id);
 }
 
-void interface(int mid){
+void interface(int mid, char id[8]){
 char x ='1';
 while(x=='1' || x=='2' || x=='3'){
     printf("Co chcesz zrobic?\n");
-    printf(" 1 <- Obsluga uzytkownika\n");
+    printf(" 1 <- Wyświetl wybraną listę\n");
     printf(" 2 <- Obsulga grup\n");
     printf(" 3 <- Wiadomosci\n");
-    printf(" 4 <- Wyloguj\n\n", );
+    printf(" 4 <- Wyloguj\n\n");
     x=getchar();
     clean_stdin();
     switch(x){
         case '1':
-            user(mid);
-            break;
+          user(mid);
+          break;
+
         case '2':
-            break;
+          group(mid);
+          break;
+
+        case '3':
+          break;
+
+        case '4':
+          strcpy(msg_pid.mtext,id);
+          msg_pid.mtype = 2000;
+          msg_pid.mpid = getpid();
+          msgsnd(mid,&msg_pid,sizeof(msg_pid)-sizeof(long),0);
+          msgrcv(mid,&msg_rec,sizeof(msg_rec)-sizeof(long),getpid(),0);
+          printf("\n%s\n",msg_rec.mtext );
+          fflush(stdout);
+          break;
+
         }
 }}
 
 void user(int mid){
+    char groupname[14];
     char y = '0';
-    char id[8];
     printf("Co chcesz zrobic?\n");
     printf(" 1 <- Wyswietl liste zalogowanych uzytkownikow\n");
     printf(" 2 <- Wyswietl liste uzytkownikow zapisancyh do danej grupy tematycznej\n\n");
@@ -65,22 +93,44 @@ void user(int mid){
     clean_stdin();
     switch (y)
     {
+        case '1':
+          msg_pid.mtype = 3000;
+          msg_pid.mpid = getpid();
+          msgsnd(mid, &msg_pid,sizeof(msg_pid)-sizeof(long),0);
+          msgrcv(mid, &msg_rec,sizeof(msg_rec)-sizeof(long),getpid(),0);
+          printf("\n%s\n",msg_rec.mtext);
+          break;
+
         case '2':
-            printf("Podaj login: \n");
-            scanf("%s",id);
-            clean_stdin();
-            break;}
+          printf("Podaj nazwe grupy: \n");
+          scanf("%s",groupname);
+          clean_stdin();
+          strcpy(msg_pid.mtext,groupname);
+          msg_pid.mtype=4000;
+          msg_pid.mpid=getpid();
+          msgsnd(mid, &msg_pid,sizeof(msg_pid)-sizeof(long),0);
+          msgrcv(mid, &msg_rec,sizeof(msg_rec)-sizeof(long),getpid(),0);
+          printf("\n%s\n",msg_rec.mtext);
+          break;}
     return;}
 
-  void zaloguj(int mid){
-    char id[8];
-    printf("Podaj login: \n");
-    scanf("%s",id);
-    clean_stdin();
-    strcpy(msg_pid.mtext,id);
-    msg_pid.mtype = 1000;
-    msg_pid.mpid = getpid();
-    msgsnd(mid,&msg_pid,sizeof(msg_pid)-sizeof(long),0);
-    msgrcv(mid,&msg_rec,sizeof(msg_rec)-sizeof(long),getpid(),0);
-    printf("%s\n",msg_rec.mtext );
+void group(int mid){
+  char y = '0';
+  printf("Co chcesz zrobic?\n");
+  printf(" 1 <- Zapisz się do grupy\n");
+  printf(" 2 <- Wypisz się z grupy\n");
+  printf(" 3 <- Wyświetl listę dostępnych grup\n\n");
+  y=getchar();
+  clean_stdin();
+  switch (y)
+  {
+      case '1':
+        break;
+
+      case '2':
+        break;
+
+      case '3':
+        break;
   }
+}
--- 141204_s.c
+++ 141204_s.c
@@ -17,6 +17,7 @@ struct msg_buf{
 
 struct msg_sen{
   long mpid;
+  int mres;
   char mtext[100];
 }msg_send;
 
@@ -24,20 +25,31 @@ struct account{
     char id[8];
     bool in;
     int pid;
-}list[3];
+}list[9];
+
+struct gr{
+  char name[14];
+  char list[9][14];
+  int count;
+}group[3];
 
 int authorize(char* login,int c );
 void logged_users_list();
+void group_users(char name[14]);
 
 int main(int argc, char* argv[]){
     int i=0;
     FILE *fp;
     fp = fopen("dane.txt","r");
     setbuf(fp, NULL);
-    while(!feof(fp)){
+    while(i<9){
         fscanf(fp,"%s",list[i].id);
         list[i].in = 0;
         i+=1;}
+    i=0;
+    while(!feof(fp)){
+      fscanf(fp,"%s",group[i].name);
+      i+=1;}
     fclose(fp);
     int mid = msgget(4444,0644|IPC_CREAT);
     while(true){
@@ -52,38 +64,85 @@ int main(int argc, char* argv[]){
           break;
 
         case 2:
-        break;
+          printf("bye\n");
+          a = authorize(my_msg.mtext,0);
+          if(a)list[a].pid=my_msg.mpid;
+          msg_send.mpid=my_msg.mpid;
+          msgsnd(mid,&msg_send,sizeof(msg_send)-sizeof(long),0);
+          break;
+
+        case 3:
+          logged_users_list();
+          msg_send.mpid=my_msg.mpid;
+          msgsnd(mid,&msg_send,sizeof(msg_send)-sizeof(long),0);
+          break;
+
+        case 4:
+          group_users(my_msg.mtext);
+          msg_send.mpid=my_msg.mpid;
+          msgsnd(mid,&msg_send,sizeof(msg_send)-sizeof(long),0);
+          break;
+
+
       }
     }
     return 0;
 }
 
 int authorize(char* login,int c ){
-    for (int i=0; i<3; i++){
+    for (int i=0; i<9; i++){
         if(strcmp(login,list[i].id)==0){
                 if(c && list[i].in==0){
                     list[i].in=1;
+                    msg_send.mres = 1;
                     strcpy(msg_send.mtext,"Pomyslnie zalogowano.\n");
                     return i;}
                 else if(!c && list[i].in==1){
                     list[i].in=0;
+                    msg_send.mres = 1;
                     strcpy(msg_send.mtext,"Pomyslnie wylogowano.\n");
                     return i;}
                 else{
-                  strcpy(msg_send.mtext,"Nie udało się wykonać operacji.\n");
+                  strcpy(msg_send.mtext,"Nie udalo sie wykonac operacji.\n");
+                  msg_send.mres = 0;
                   return 0;}
                 }
         }
     strcpy(msg_send.mtext,"Nie ma uzytkownika o takim loginie.\n");
+    msg_send.mres = 0;
     return 0;}
 
 void logged_users_list(){
-  printf("Zalogowani użytkownicy:\n");
-  for(int i=0;i<3;i++)
+  strcpy(msg_send.mtext,"Zalogowani uzytkownicy:\n");
+  for(int i=0;i<9;i++)
   {
     if(list[i].in==1)
     {
-      printf("%s\n",list[i].id );
+      strcat(msg_send.mtext,list[i].id);
+      strcat(msg_send.mtext,"\n");
     }
   }
+}
+
+void group_users(char name[14]){
+  int index;
+  for (int i=0;i<3;i++){
+    if(strcmp(name,group[i].name)==0){
+        index=i;
+        if(group[index].count){
+          strcpy(msg_send.mtext,"Uzytkownicy nalezacy do grupy ");
+          strcat(msg_send.mtext,name);
+          strcat(msg_send.mtext,":\n");
+          for(int j=0;j<group[index].count;j++){
+            strcat(msg_send.mtext,group[index].list[j]);
+            strcat(msg_send.mtext,"\n");}
+          return;
+        }else{
+          strcpy(msg_send.mtext,"Zaden uzytkownik nie nalezy do tej grupy.\n");
+          return;
+        }
+    }
+  }
+  strcpy(msg_send.mtext,"Nie znaleziono takiej grupy.\n");
+  return;
 }
